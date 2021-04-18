a42b1aa6ab12ed654fab8be013a87623d33d9aac
chinedu umukoro <umukoro@chaka.ng>
2021-04-18 21:12:00 +0100

    added screens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PrivateRoute from './components/routes/PrivateRoute';
+import ForgotPasswordScreen from './components/screens/ForgotPassword/ForgotPasswordScreen';
+import LoginScreen from './components/screens/Login/LoginSceen';
+import PrivateScreen from './components/screens/PrivateScreen/PrivateScreen';
+import RegisterScreen from './components/screens/Register/RegisterSceen';
+import ResetPasswordScreen from './components/screens/ResetPassword/ResetPasswordScreen';
+
 
 const App = () => {
   return (
-    <div className="App">
-     React app
-    </div>
+    <Router>
+
+      <div className="App">
+        <Switch>
+          <PrivateRoute exact path="/" component={PrivateScreen} />
+          <Route exact path="/login" component={LoginScreen}/>
+          <Route exact path="/register" component={RegisterScreen}/>
+          <Route exact path="/forgotpassword" component={ForgotPasswordScreen}/>
+          <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
